e712b20674fda663a42ddec05a5f48b49c3b1fe3
Eka Suwartadi <eka.suwartadi@tieto.com>
2020-06-27 22:53:05 +0200

    update calculator.

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,16 +1,68 @@
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <stack>
+#include <string>
 
-int main() {
-    std::stack<int> s;
-    s.push(10);
-    s.push(20);
-    s.push(30);
+void evaluateStackTops(std::stack<int>& numbers, std::stack<char>& operations)
+{
+    int operand1 = 0, operand2 = 0;
+    operand2 = numbers.top();
+    numbers.pop();
+    operand1 = numbers.top();
+    numbers.pop();
+    switch (operations.top())
+    {
+        case '+':
+            numbers.push(operand1 + operand2);
+            break;
+        case '-':
+            numbers.push(operand1 - operand2);
+            break;
+        case '*':
+            numbers.push(operand1 * operand2);
+            break;
+        case '/':
+            numbers.push(operand1 / operand2);
+            break;
+    }
+    operations.pop();
+}
 
-    while(!s.empty()){
-        std::cout << s.top() << '\n';
-        s.pop();
+double readAndEvaluate(const std::string& ins)
+{
+    const char DECIMAL = '.';
+    const char RIGHT_PARENTHESIS = ')';
+
+    std::stack<int> numbers;
+    std::stack<char> operations;
+
+
+    for(int i=0; i < ins.length(); i++)
+    {
+        if(isdigit(ins[i]) || (ins[i] == DECIMAL))
+        {
+            numbers.push(ins[i]-'0');
+        }
+        else if(strchr("+-*/",ins[i]) != NULL)
+        {
+            operations.push(ins[i]);
+        }
+        else if(ins[i] == RIGHT_PARENTHESIS)
+        {
+            evaluateStackTops(numbers,operations);
+        }
     }
 
-    return 0;
+    return numbers.top();
+}
+int main ()
+{
+    double answer;
+    //std::cout << "Type a fully parenthesized arithmetic expression:" << std::endl;
+    answer = readAndEvaluate(std::string("(((3*2)+(1+2))/3)"));
+    std::cout << "That evaluates to " << answer << std::endl;
+
+    return EXIT_SUCCESS;
 }
